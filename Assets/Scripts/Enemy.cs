e8f43b877673d2cd0458b5878e2dfd8efaab7122
Dante Barbosa-Toney <dtoney136@gmail.com>
2024-09-27 19:17:49 -0700

    Basic Enemy AI
    Basic Enemy AI is implemented via states
    >Enemies will wander, chase, attack, and die
    >Ranged attacking needs to be implemented
    (Maybe we make a scriptable object template for spells that can be used by Player and Enemies????)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,50 +4,115 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private string[] words;
-    private string buffer = "";
+    private enum STATE { WANDER, CHASE, ATTACK, DIE };
+    public float wanderSpeed;
+    public float chaseSpeed;
+    public float minWanderTime;
+    public float maxWanderTime;
+    public float sightDistance;
+    public float strikeDistance;
+    public float rotationSpeed;
+    public LayerMask willAttackLayer;
 
+    private STATE _currentState;
+    private float _currentWanderTime;
+    private Vector2 _currentDirection;
+    private GameObject _currentTarget;
+    private Rigidbody2D _myRigidbody2D;
+
+    void Start()
+    {
+        _myRigidbody2D = GetComponent<Rigidbody2D>();
+        EnterStateWander();
+    }
     void Update()
     {
-        foreach (char c in Input.inputString)
+        switch (_currentState)
         {
-            switch (c)
-            {
-                // cast
-                case '\n':
-                case '\r':
-
-                    // placeholders for now, should fire a particle or decrease health or whatever
-                    if (wordCompare(buffer))
-                    {
-                        print("Correct Spell");
-                        Destroy(gameObject);
-                    }
-                    else
-                    {
-                        print("Wrong Spell");
-                    }
-                    buffer = "";
-                    break;
-
-                // delete
-                case '\b':
-                    buffer = "";
-                    break;
-
-                default:
-                    buffer += c;
-                    break;
-            }
+            case STATE.WANDER:
+                UpdateWander();
+                break;
+            case STATE.CHASE:
+                UpdateChase();
+                break;
+            case STATE.ATTACK:
+                UpdateAttack();
+                break;
+            case STATE.DIE:
+                UpdateDie();
+                break;
         }
     }
-
-    private bool wordCompare(string word)
+    private void EnterStateWander()
     {
-        foreach (string curWord in words)
+        _currentState = STATE.WANDER;
+        _currentDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        _currentWanderTime = Random.Range(minWanderTime, maxWanderTime);
+    }
+    private void UpdateWander()
+    {
+        _myRigidbody2D.velocity = _currentDirection * wanderSpeed;
+        _currentWanderTime -= Time.deltaTime;
+        if (_currentWanderTime <= 0f) EnterStateWander();
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, _currentDirection, sightDistance, willAttackLayer.value);
+        if (hit.collider != null)
         {
-            if (word == curWord) return true;
+            Debug.Log("Attack");
+            EnterStateChase(hit.collider.gameObject);
         }
-        return false;
+    }
+    private void EnterStateChase(GameObject target)
+    {
+        _currentState = STATE.CHASE;
+        _currentTarget = target;
+        _currentDirection = (target.transform.position - transform.position).normalized;
+    }
+    private void UpdateChase()
+    {
+        Vector2 targetDirection = (_currentTarget.transform.position - transform.position).normalized;
+        _currentDirection = Vector3.RotateTowards(_currentDirection.normalized,
+        targetDirection, rotationSpeed * Mathf.Deg2Rad * Time.deltaTime, 0f).normalized;
+        _myRigidbody2D.velocity = _currentDirection * chaseSpeed;
+
+        float targetDistance = Vector3.Distance(_currentTarget.transform.position, transform.position);
+        if (targetDistance <= strikeDistance)
+        {
+            EnterStateAttack();
+        }
+        else if (targetDistance > sightDistance || Vector2.Angle(_currentDirection, targetDirection) > 30f)
+        {
+            _currentState = STATE.WANDER;
+        }
+    }
+    private void EnterStateAttack()
+    {
+        _currentState = STATE.ATTACK;
+        _myRigidbody2D.velocity = Vector2.zero;
+    }
+    private void UpdateAttack()
+    {
+        //Nothing to do here until the attack ends!
+    }
+    public void DoAttack()
+    {
+        //Nothing for now
+    }
+    public void AttackOver()
+    {
+        EnterStateWander();
+    }
+    public void Die()
+    {
+        EnterStateDie();
+    }
+    private void EnterStateDie()
+    {
+        _currentState = STATE.DIE;
+        _myRigidbody2D.velocity = Vector2.zero;
+        GetComponent<Collider2D>().enabled = false;
+    }
+    private void UpdateDie()
+    {
+        Destroy(gameObject);
     }
 }
