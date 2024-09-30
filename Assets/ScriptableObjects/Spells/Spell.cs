cf4456cdc2e5bd3a089d54b8cafd5fcff8535f24
Dante Barbosa-Toney <dtoney136@gmail.com>
2024-09-30 16:19:31 -0400

    Spell Structure

---
+++ Assets/ScriptableObjects/Spells/Spell.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpellStructure {Eement, Projectile}
+[CreateAssetMenu]
+public class Spell : ScriptableObject
+{
+    [SerializeField]
+    private Sprite _projectileType;
+    public Sprite ProjectileType { get => _projectileType; }
+
+    [SerializeField]
+    private string _spellPart;
+    public string SpellPart { get => _spellPart;}
+
+    [SerializeField]
+    private ParticleSystem _particleSystem;
+    public ParticleSystem ParticleSystem { get => _particleSystem; }
+
+    [SerializeField]
+    private Color _element;
+    public Color Element { get => _element; }
+}
